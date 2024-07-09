f4b842596c7c10b953abc57368adc74eaf0b0548
njabulo <njebzeliny@gmail.com>
2024-07-09 06:41:45 +0200

    DTO

---
+++ Shared/DTO/User/ForgotPasswordDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DTO.User
+{
+    public class ForgotPasswordDto
+    {
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? ClientURI { get; set; }
+    }
+}
---
+++ Shared/DTO/User/RegistrationResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.DTO.User
+{
+    public class RegistrationResponseDto
+    {
+        public bool IsSuccessfulRegistration { get; set; }
+        public IEnumerable<string>? Errors { get; set; }
+    }
+}
