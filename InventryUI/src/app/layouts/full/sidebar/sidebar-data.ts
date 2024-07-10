3c3f32899ae9f2e07f97be99a2988f0f86cb8a70
njabulo <njebzeliny@gmail.com>
2024-07-10 09:54:42 +0200

    nav list desgin

--- InventryUI/src/app/layouts/full/sidebar/sidebar-data.ts
+++ InventryUI/src/app/layouts/full/sidebar/sidebar-data.ts
@@ -10,43 +10,87 @@ export const navItems: NavItem[] = [
     route: '/dashboard',
   },
   {
-    navCap: 'Products',
+    navCap: 'Inventory Management',
   },
   {
-    displayName: 'Product',
-    iconName: 'poker-chip',
-    route: '/ui-components/roles',
+    displayName: 'Devices',
+    iconName: 'devices',
+    route: '/inventory/devices',
   },
   {
     displayName: 'Category',
     iconName: 'rosette',
-    route: '/ui-components/users',
+    route: '/inventory/category',
   },
-
   {
     displayName: 'Brand',
     iconName: 'list',
-    route: '/ui-components/lists',
+    route: '/inventory/brand',
+  },
+  {
+    displayName: 'Suppliers',
+    iconName: 'storefront',
+    route: '/inventory/suppliers',
   },
   {
     navCap: 'User Management',
   },
   {
-    displayName: 'User list',
-    iconName: 'poker-chip',
-    route: '/ui-components/roles',
+    displayName: 'User',
+    iconName: 'user-check',
+    route: '/user-management/user',
   },
   {
-    displayName: 'Role',
-    iconName: 'poker-chip',
-    route: '/ui-components/roles',
+    navCap: 'Device Assignment',
   },
   {
-    navCap: 'Report',
+    displayName: 'Assign Devices',
+    iconName: 'device-hub',
+    route: '/device-assignment/assign',
   },
   {
-    displayName: 'Product Report',
-    iconName: 'poker-chip',
-    route: '/ui-components/roles',
-  }
+    displayName: 'Assigned Devices',
+    iconName: 'assignment',
+    route: '/device-assignment/list',
+  },
+
+  {
+    navCap: 'Employee and Office',
+  },
+  {
+    displayName: 'Employees',
+    iconName: 'users',
+    route: '/employee-office/employees',
+  },
+  {
+    displayName: 'Offices',
+    iconName: 'building',
+    route: '/employee-office/offices',
+  },
+  {
+    navCap: 'Asset Management',
+  },
+  {
+    displayName: 'Maintenance',
+    iconName: 'calendar',
+    route: '/asset-management/maintenance',
+  },
+  {
+    displayName: 'Service History',
+    iconName: 'history',
+    route: '/asset-management/service-history',
+  },
+  {
+    displayName: 'Faulty Devices',
+    iconName: 'alert-triangle',
+    route: '/inventory/faulty-devices',
+  },
+  {
+    navCap: 'Reports and Analytics',
+  },
+  {
+    displayName: 'Reports',
+    iconName: 'bar-chart',
+    route: '/reports',
+  },
 ];
