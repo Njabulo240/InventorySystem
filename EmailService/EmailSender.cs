fdb9340dde17d61d18d56400cd9a2a4302f2a2f8
njabulo <njebzeliny@gmail.com>
2024-07-24 14:10:10 +0200

    61

--- EmailService/EmailSender.cs
+++ EmailService/EmailSender.cs
@@ -36,7 +36,7 @@ namespace EmailService
             emailMessage.To.AddRange(message.To);
             emailMessage.Subject = message.Subject;
 
-            var bodyBuilder = new BodyBuilder { HtmlBody = string.Format("<h2 style='color:red;'>{0}</h2>", message.Content) };
+            var bodyBuilder = new BodyBuilder { HtmlBody = string.Format("<h3 style='color:red;'>{0}</h3>", message.Content) };
 
             if (message.Attachments != null && message.Attachments.Any())
             {
